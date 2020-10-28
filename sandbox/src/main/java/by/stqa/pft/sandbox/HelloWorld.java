d8baae992c2add00762f2128c7641308b1279c62
sergey.adamtsevich <user.sergri@gmail.com>
2020-10-28 22:46:22 +0300

    create package

--- sandbox/src/main/java/by/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/HelloWorld.java
@@ -1,4 +1,6 @@
-public class Helloworld {
+package by.stqa.pft.sandbox;
+
+public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("You are the best!");
     }
