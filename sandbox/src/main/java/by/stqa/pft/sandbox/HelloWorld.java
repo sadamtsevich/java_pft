2b017b4ed422e13bbd6dd73e65e5a13dc5149299
sadamtsevich <73597439+sadamtsevich@users.noreply.github.com>
2020-10-28 22:47:33 +0300

    Update HelloWorld.java

--- sandbox/src/main/java/by/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/HelloWorld.java
@@ -2,6 +2,6 @@ package by.stqa.pft.sandbox;
 
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("You are the best!");
+        System.out.println("You are the best");
     }
 }
