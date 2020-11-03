7746db1077b51577d314c80176d11fe2c5dfbbf1
sergey.adamtsevich <user.sergri@gmail.com>
2020-11-03 14:22:42 +0300

    Creared new classes

--- sandbox/src/main/java/Helloworld.java
+++ sandbox/src/main/java/Helloworld.java
@@ -2,24 +2,14 @@ public class Helloworld {
     public static void main(String args[]) {
 
         hello("world");
-        double l = 5;
-        System.out.println("Area with side " + l + " = " + area(l));
-        double c = 3;
-        double d = 5;
-        System.out.println("Area with side " + c + " and " + d + " = " + area(c, d));
+        Square s = new Square(5);
+        System.out.println("Area with side " + s.l + " = " + s.area());
+        Rectangle r = new Rectangle(3, 5);
+        System.out.println("Area with side " + r.a + " and " + r.b + " = " + r.area());
 
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello " + somebody + "!");
     }
-
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
-    }
-
 }
