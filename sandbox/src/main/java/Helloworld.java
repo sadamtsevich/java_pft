e4883bee1dd94391dcfe3ff382eb9bd688756c27
sergey.adamtsevich <user.sergri@gmail.com>
2020-11-03 11:46:27 +0300

    added several functions

--- sandbox/src/main/java/Helloworld.java
+++ sandbox/src/main/java/Helloworld.java
@@ -1,6 +1,25 @@
 public class Helloworld {
     public static void main(String args[]) {
 
-        System.out.println("You are the best!");
+        hello("world");
+        double l = 5;
+        System.out.println("Area with side " + l + " = " + area(l));
+        double c = 3;
+        double d = 5;
+        System.out.println("Area with side " + c + " and " + d + " = " + area(c, d));
+
     }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
 }
